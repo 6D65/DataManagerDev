36c9d322febd6fe5ce1a88429201310a74170f71
The Dude <u6du65@gmail.com>
2015-03-13 23:55:22 +0200

    The first commit. May the force be with us.

---
+++ SampleTestProject/TestClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestMonkeys.DataManager;
+using System.Threading.Tasks;
+
+namespace SampleTestProject
+{
+    [TestClass]
+    public class TestClass
+    {
+        #region Fields
+        DataManagerClient _dm;
+        #endregion Fields
+
+        [TestMethod]
+        public void FirstTestMethod()
+        {
+            //_dm.Documents
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _dm = new DataManagerClient();
+        }
+    }
+}
