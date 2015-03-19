bfdf83b184e836f79a092dde48be39979ee604b7
The Dude <u6du65@gmail.com>
2015-03-20 00:14:14 +0200

    Started working on the Mock Api
    Started the Mock Api to develop the DataManager against. NancyFX does a
    very good job for simple mock services.

--- ApiSampleService/Program.cs
+++ ApiSampleService/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Nancy;
+using Nancy.Hosting.Self;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,39 @@ namespace ApiSampleService
     {
         static void Main(string[] args)
         {
+            var uri = new Uri("http://localhost:8888");
+            HostConfiguration hostConfigs = new HostConfiguration();
+            hostConfigs.UrlReservations.CreateAutomatically = true;
+
+            using (var host = new NancyHost(new Uri("http://localhost:8000"), new DefaultNancyBootstrapper(), hostConfigs))
+            {
+                host.Start();
+
+                Console.WriteLine("Mock Server Running..");
+                Console.WriteLine("Press any [Enter] to close the host.");
+                Console.ReadLine();
+            }
         }
+    }
+
+    public class ResourceModule : NancyModule
+    {
+        public ResourceModule()
+        {
+            Company company = new Company { Address = "BananaStreet", Name = "Company Name" };
+
+            Get["/companies/{companyId:int}"] = parameters =>
+            {
+                company.Id = parameters.companyId;
+                return Response.AsJson(JsonConvert.SerializeObject(company));
+            };
+        }
+    }
+
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Id { get; set; }
     }
 }
