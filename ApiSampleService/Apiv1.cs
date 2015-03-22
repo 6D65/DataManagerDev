e22d17b1a4b6e795c2087d8997c03974d75eb784
The Dude <u6du65@gmail.com>
2015-03-22 15:31:35 +0200

    Added the ApiKey and Token auth mechanisms to the mock Api
    Extended the MockApi to use the ApiKey(Stateless Authentication) and the
    Token Authentication. Also, moved the Api endpoints into their own
    classes.

---
+++ ApiSampleService/Apiv1.cs
@@ -0,0 +1,93 @@
+﻿using Nancy;
+using Nancy.Authentication.Token;
+using Nancy.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiSampleService.Api.v1
+{
+    public class CompaniesApi : NancyModule
+    {
+        public CompaniesApi()
+        {
+            Get["/api/v1/companies/{companyId:int}"] = parameters =>
+            {
+                Company company = new Company();
+                company.Id = parameters.companyId;
+                company.Name = string.Format("Company {0} Name", parameters.companyId);
+                company.Address = string.Format("Company {0} Address", parameters.companyId);
+
+                SingleResult result = new SingleResult { Result = company, Success = true };
+                return Response.AsJson(result);
+            };
+
+            Get["/api/v1/companies"] = parameters =>
+            {
+                List<object> companiesResult = new List<object>();
+                for (int i = 0; i < 100; i++)
+                {
+                    Company company = new Company();
+                    company.Id = i;
+                    company.Name = string.Format("Company {0} Name", i);
+                    company.Address = string.Format("Company {0} Addres", i);
+                    companiesResult.Add(company);
+                }
+                ArrayResult result = new ArrayResult { Result = companiesResult, Success = true };
+                return Response.AsJson(result);
+            };
+
+            Post["/api/v1/companies"] = _ =>
+            { 
+                this.RequiresAuthentication();
+            };
+
+        }
+    }
+
+    public class AuthModule : NancyModule
+    {
+        public AuthModule(ITokenizer tokenizer)
+            : base("/auth")
+        {
+            Post["/token"] = x =>
+            {
+                IUserIdentity indentity = new BasicIdentity { UserName = "admin", Claims = new List<string> { "Money", "Power" } };
+                var token = tokenizer.Tokenize(indentity, Context);
+                return new { Token = token };
+            };
+
+            Post["/apikey"] = x => 
+            {
+                return new { ApiKey = "c5c16da7-e0fc-4e14-a522-3dab15a7a593" };
+            };
+        }
+    }
+
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Id { get; set; }
+    }
+
+    public class SingleResult
+    {
+        public bool Success { get; set; }
+        public object Result { get; set; }
+    }
+
+    public class ArrayResult
+    {
+        public bool Success { get; set; }
+        public List<object> Result { get; set;}
+    }
+
+    public class BasicIdentity : IUserIdentity
+    {
+        public string UserName { get; set; }
+        public IEnumerable<string> Claims { get; set; }
+    }
+}
--- ApiSampleService/Program.cs
+++ ApiSampleService/Program.cs
@@ -1,5 +1,10 @@
 ﻿using Nancy;
+using Nancy.Authentication.Stateless;
+using Nancy.Authentication.Token;
+using Nancy.Bootstrapper;
 using Nancy.Hosting.Self;
+using Nancy.Security;
+using Nancy.TinyIoc;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -13,11 +18,11 @@ namespace ApiSampleService
     {
         static void Main(string[] args)
         {
-            var uri = new Uri("http://localhost:8888");
+            var uri = new Uri("http://localhost:8000");
             HostConfiguration hostConfigs = new HostConfiguration();
             hostConfigs.UrlReservations.CreateAutomatically = true;
 
-            using (var host = new NancyHost(new Uri("http://localhost:8000"), new DefaultNancyBootstrapper(), hostConfigs))
+            using (var host = new NancyHost(uri, new ApiKeyAuthBoostrapper(), hostConfigs))
             {
                 host.Start();
 
@@ -26,26 +31,62 @@ namespace ApiSampleService
                 Console.ReadLine();
             }
         }
-    }
 
-    public class ResourceModule : NancyModule
-    {
-        public ResourceModule()
+        public class TokenAuthBootstrapper : DefaultNancyBootstrapper
         {
-            Company company = new Company { Address = "BananaStreet", Name = "Company Name" };
-
-            Get["/companies/{companyId:int}"] = parameters =>
+            protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
             {
-                company.Id = parameters.companyId;
-                return Response.AsJson(company);
-            };
+                TokenAuthentication.Enable(pipelines, new TokenAuthenticationConfiguration(container.Resolve<ITokenizer>()));
+            }
         }
-    }
 
-    public class Company
-    {
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public int Id { get; set; }
+        public class ApiKeyAuthBoostrapper : DefaultNancyBootstrapper
+        {
+            protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+            {
+                var config = new StatelessAuthenticationConfiguration(nancyContext => 
+                {
+                    var apiKey = nancyContext.Request.Headers["x-ApiKey"].FirstOrDefault();
+
+                    if (apiKey == null) { return null; }
+
+                    // In case the ApiKey is passed as a query parameter.
+                    //if (!nancyContext.Request.Query.apiKey.HasValue) { return null; }
+                    //var apiKey = (string)nancyContext.Request.Query.apiKey.Value;
+
+                    return DataBaseMock.GetUserFromApiKey(apiKey);
+                });
+
+                AllowAccessToConsumingSite(pipelines);
+                StatelessAuthentication.Enable(pipelines, config);
+            }
+        }
+
+        static void AllowAccessToConsumingSite(IPipelines pipelines)
+        {
+            pipelines.AfterRequest.AddItemToEndOfPipeline(x =>
+            {
+                x.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+                x.Response.Headers.Add("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT,OPTIONS");
+            });
+        }
+
+        public class DataBaseMock
+        {
+            public static AppUserIndentity GetUserFromApiKey(string apiKey)
+            {
+                if (apiKey == "c5c16da7-e0fc-4e14-a522-3dab15a7a593") 
+                {
+                    return new AppUserIndentity { UserName = "admin", Claims = new List<string> { "god" } };
+                }
+                return null;
+            }
+        }
+
+        public class AppUserIndentity : IUserIdentity
+        {
+            public IEnumerable<string> Claims { get; set; }
+            public string UserName { get; set; }
+        }
     }
 }
